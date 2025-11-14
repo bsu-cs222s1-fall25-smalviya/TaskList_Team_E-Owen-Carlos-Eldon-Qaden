dd204e9ffe0c8f433edaa0d579f857d8477b42de
owenbgriffin-collab <owenbgriffin@gmail.com>
2025-11-13 23:30:27 -0500

    Adding test cases

---
+++ src/test/java/edu/bsu/cs222/project2menu/TaskList_Tests.java
@@ -0,0 +1,22 @@
+package edu.bsu.cs222.project2menu;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskList_Tests {
+
+    @Test
+    public void launchTest(){
+        launchTest();
+    }
+
+    @Test
+    public void photoTest(){
+        photoTest();
+    }
+
+    @Test
+    public void applicationTest(){
+        applicationTest();
+    }
+
+}
