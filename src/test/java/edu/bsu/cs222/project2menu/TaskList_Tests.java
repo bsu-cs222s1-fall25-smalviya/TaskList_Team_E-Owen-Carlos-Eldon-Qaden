02154749f5fafbbe65c35eb02add5098835ac7f7
owenb <owenbgriffin@gmail.com>
2025-12-03 22:56:51 -0500

    5 test cases(all five are passing)

--- src/test/java/edu/bsu/cs222/project2menu/TaskList_Tests.java
+++ src/test/java/edu/bsu/cs222/project2menu/TaskList_Tests.java
@@ -1,9 +1,6 @@
 package edu.bsu.cs222.project2menu;
 
-import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskList_Tests {
     //Test case for our Photo Class
@@ -12,5 +9,28 @@ public class TaskList_Tests {
         Photo photo = new Photo();
         photo.saveScreenshot();
     }
+    //Test case for our Format class
+    @Test
+    public void test_TaskList_Test2() {
+        Format format = new Format();
+        format.getClass();
+    }
+    //Test case for our Launcher class
+    @Test
+    public void test_TaskList_Test3() {
+        Launcher launcher = new Launcher();
+        launcher.getClass();
+    }
     //Test case for our TaskApplication class
+    @Test
+    public void test_TaskList_Test4() {
+        TaskApplication application = new TaskApplication();
+        application.getClass();
+    }
+    //Test case for our TaskListController class
+    @Test
+    public void test_TaskList_Test5() {
+        TaskListController controller = new TaskListController();
+        controller.getClass();
+    }
 }
